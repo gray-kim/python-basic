94ef2430618fe74046d477fda24c9b03ddd40284
gray-kim <egoist3.work@gmail.com>
2021-03-11 22:57:23 +0900

    20210311 연습
    20210311 연습

--- walk.py
+++ walk.py
@@ -1,11 +1,36 @@
 import turtle as t
-import random
+
+
+def turn_right():
+    t.setheading(0)
+    t.forward(10)
+
+
+def turn_up():
+    t.setheading(90)
+    t.forward(10)
+
+
+def turn_left():
+    t.setheading(180)
+    t.forward(10)
+
+
+def turn_down():
+    t.setheading(270)
+    t.forward(10)
+
+
+def blank():
+    t.clear()
+
 
 t.shape("turtle")
 t.speed(0)
-
-for x in range(500) :
-    a = random.randint(1, 360)
-    t.setheading(a)
-    b = random.randint(1, 20)
-    t.forward(b)
+t.onkeypress(turn_right, "Right")
+t.onkeypress(turn_up, "Up")
+t.onkeypress(turn_left, "Left")
+t.onkeypress(turn_down, "Down")
+t.onkeypress(blank, "Escape")
+t.listen()
+t.mainloop()
